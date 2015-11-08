940de301478e92bb55c6ff83cd0444bfe5f9bb74
spencerhh <spencer.hall16@gmail.com>
2015-11-08 15:42:23 -0800

    updated some swift code to 2.0
    update

--- BCP Live/ViewController.swift
+++ BCP Live/ViewController.swift
@@ -29,25 +29,25 @@ class ViewController: UIViewController {
     
     @IBAction func signUpAction(sender: AnyObject) {
         
-        var studentID = self.studentIDField.text
-        var fullName = self.fullNameField.text
+        let studentID = self.studentIDField.text
+        let fullName = self.fullNameField.text
     
         
         // Validate the text fields
-        if count(studentID) < 6 {
-            var alert = UIAlertView(title: "Invalid", message: "Input Valid Student ID", delegate: self, cancelButtonTitle: "OK")
+        if studentID!.characters.count < 6 {
+            let alert = UIAlertView(title: "Invalid", message: "Input Valid Student ID", delegate: self, cancelButtonTitle: "OK")
             alert.show()
             
-        } else if count(fullName) < 8 {
-            var alert = UIAlertView(title: "Invalid", message: "Input First and Last name separated by a space", delegate: self, cancelButtonTitle: "OK")
+        } else if fullName!.characters.count < 8 {
+            let alert = UIAlertView(title: "Invalid", message: "Input First and Last name separated by a space", delegate: self, cancelButtonTitle: "OK")
             alert.show()
             
         } else {
             // Run a spinner to show a task in progress
-            var spinner: UIActivityIndicatorView = UIActivityIndicatorView(frame: CGRectMake(0, 0, 150, 150)) as UIActivityIndicatorView
+            let spinner: UIActivityIndicatorView = UIActivityIndicatorView(frame: CGRectMake(0, 0, 150, 150)) as UIActivityIndicatorView
             spinner.startAnimating()
             
-            var alert = UIAlertView(title: "Success", message: "Great JOB!", delegate: self, cancelButtonTitle: "OK")
+            let alert = UIAlertView(title: "Success", message: "Great JOB!", delegate: self, cancelButtonTitle: "OK")
             alert.show()
         
             
