0110ae48bf2833e745e840625f8bf0b72b25a3fc
Ryan E <ryandebner47@gmail.com>
2015-11-08 15:08:09 -0800

    Initial Commit

---
+++ BCP Live/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BCP Live
+//
+//  Created by Ryan Ebner on 11/8/15.
+//  Copyright (c) 2015 Ryan Ebner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
