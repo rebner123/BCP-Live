9750d1981b506b31e2ea3c279ffea7370ef10b4d
Ryan E <ryandebner47@gmail.com>
2015-11-08 15:31:21 -0800

    Added Two Text Fields
    Added Student ID Field and Full Name Text Field

--- BCP Live/ViewController.swift
+++ BCP Live/ViewController.swift
@@ -21,5 +21,39 @@ class ViewController: UIViewController {
     }
 
 
+
+    
+    @IBOutlet weak var studentIDField: UITextField!
+    @IBOutlet weak var fullNameField: UITextField!
+    
+    
+    @IBAction func signUpAction(sender: AnyObject) {
+        
+        var studentID = self.studentIDField.text
+        var fullName = self.fullNameField.text
+    
+        
+        // Validate the text fields
+        if count(studentID) < 6 {
+            var alert = UIAlertView(title: "Invalid", message: "Input Valid Student ID", delegate: self, cancelButtonTitle: "OK")
+            alert.show()
+            
+        } else if count(fullName) < 8 {
+            var alert = UIAlertView(title: "Invalid", message: "Input First and Last name separated by a space", delegate: self, cancelButtonTitle: "OK")
+            alert.show()
+            
+        } else {
+            // Run a spinner to show a task in progress
+            var spinner: UIActivityIndicatorView = UIActivityIndicatorView(frame: CGRectMake(0, 0, 150, 150)) as UIActivityIndicatorView
+            spinner.startAnimating()
+            
+            var alert = UIAlertView(title: "Success", message: "Great JOB!", delegate: self, cancelButtonTitle: "OK")
+            alert.show()
+        
+            
+            
+        }
+    }
+    
 }
 
