d6741b64a775520b4482605a5d213e09bb5055e9
spencerhh <spencer.hall16@gmail.com>
2015-11-08 16:58:49 -0800

    updated login
    login is now entirely based on existence of dictionary values

--- BCP Live/ViewController.swift
+++ BCP Live/ViewController.swift
@@ -19,41 +19,49 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-
     
+    
+    
+
+    // login page buttons
     @IBOutlet weak var studentIDField: UITextField!
     @IBOutlet weak var fullNameField: UITextField!
     
     
-    @IBAction func signUpAction(sender: AnyObject) {
+    // login
+    @IBAction func login(sender: AnyObject) {
         
+        // student dictionary
+        let students = [
+            "216158": "Spencer Hall",
+            "217182": "Ryan Ebner",
+            "216356": "Eric Sichak",
+            "217092": "Tyler Burch",
+            "216091": "Corey Camacho-Light"
+        ]
+
         let studentID = self.studentIDField.text
         let fullName = self.fullNameField.text
-    
         
-        // Validate the text fields
-        if studentID!.characters.count < 6 {
-            let alert = UIAlertView(title: "Invalid", message: "Input Valid Student ID", delegate: self, cancelButtonTitle: "OK")
+        
+        /* There are two ways to fail the login:
+        1. the student ID doesn't exist
+        2. the student ID exists but doesn't match the name
+        */
+        
+        if students[studentID!] == nil{ // case 1
+            let alert = UIAlertView(title: "Login Failed", message: "ID not found", delegate: self, cancelButtonTitle: "Ok")
             alert.show()
-            
-        } else if fullName!.characters.count < 8 {
-            let alert = UIAlertView(title: "Invalid", message: "Input First and Last name separated by a space", delegate: self, cancelButtonTitle: "OK")
+        }
+        else if fullName! != students[studentID!] { // case 2
+            let alert = UIAlertView(title: "Login Failed", message: "Name does not match ID", delegate: self, cancelButtonTitle: "Ok")
             alert.show()
-            
-        } else {
-            // Run a spinner to show a task in progress
-            let spinner: UIActivityIndicatorView = UIActivityIndicatorView(frame: CGRectMake(0, 0, 150, 150)) as UIActivityIndicatorView
-            spinner.startAnimating()
-            
-            let alert = UIAlertView(title: "Success", message: "Great JOB!", delegate: self, cancelButtonTitle: "OK")
+        }
+        else {
+            let alert = UIAlertView(title: "Login Successful", message: nil, delegate: self, cancelButtonTitle: "Ok")
             alert.show()
         
-            
             
         }
     }
-    
 }
-
