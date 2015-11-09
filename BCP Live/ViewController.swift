0b7ff6a19acbb913b793ad53944c13f8f03b07ef
spencerhh <spencer.hall16@gmail.com>
2015-11-08 17:57:12 -0800

    updated ui, new file for dict
    I made the first page less awful looking and created a swift file to
    hold the whole dictionary for the login

--- BCP Live/ViewController.swift
+++ BCP Live/ViewController.swift
@@ -21,34 +21,24 @@ class ViewController: UIViewController {
     }
     
     
-    
+
 
     // login page buttons
     @IBOutlet weak var studentIDField: UITextField!
     @IBOutlet weak var fullNameField: UITextField!
     
-    
     // login
     @IBAction func login(sender: AnyObject) {
         
-        // student dictionary
-        let students = [
-            "216158": "Spencer Hall",
-            "217182": "Ryan Ebner",
-            "216356": "Eric Sichak",
-            "217092": "Tyler Burch",
-            "216091": "Corey Camacho-Light"
-        ]
 
         let studentID = self.studentIDField.text
         let fullName = self.fullNameField.text
-        
         
         /* There are two ways to fail the login:
         1. the student ID doesn't exist
         2. the student ID exists but doesn't match the name
         */
-        
+    
         if students[studentID!] == nil{ // case 1
             let alert = UIAlertView(title: "Login Failed", message: "ID not found", delegate: self, cancelButtonTitle: "Ok")
             alert.show()
@@ -60,8 +50,6 @@ class ViewController: UIViewController {
         else {
             let alert = UIAlertView(title: "Login Successful", message: nil, delegate: self, cancelButtonTitle: "Ok")
             alert.show()
-        
-            
         }
     }
 }
---
+++ BCP Live/studentDict.swift
@@ -0,0 +1,42 @@
+//
+//  studentDict.swift
+//  BCP Live
+//
+//  Created by Spencer on 11/8/15.
+//  Copyright © 2015 Ryan Ebner. All rights reserved.
+//
+
+import Foundation
+
+/* Dictionary
+This dictionary is used in the login
+It has its own file because it will hold ~1600 keys and ~1600 names
+*/
+
+let students = [
+    "216001": "Spencer Hall",
+    "216002": "Ryan Ebner",
+    "216003": "Brian Oneil",
+    "216004": "Corey Camacho-Light",
+    "216005": "John Winchester",
+    "216006": "Jacob Diaz",
+    "216007": "Sean Becker",
+    "216008": "Dominick Daprile",
+    "216009": "Dylan Carruba",
+    "216010": "Eric Sichak",
+    "216011": "Robby Munne",
+    "216012": "Jack Shea",
+    "216013": "Jack Hickman",
+    "216014": "Kevin Tucker",
+    "216015": "James Gipson",
+    "216016": "Joey Rubino",
+    "216017": "Andreas Guerrero",
+    "216018": "Kyle Dean",
+    "216019": "Coleman Shaheen",
+    "216020": "Jared Hall",
+    "216021": "Nolan Thomas",
+    "216022": "Perry Wreaks",
+    "216023": "Kobe Carr",
+    "216024": "Nathan Duong",
+    "216025": "Ryan Hayden",
+]
